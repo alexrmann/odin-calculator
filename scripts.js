62c7cc6fe5853e2acc504568f4bf46d0c5deb199
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2025-01-27 20:20:07 -0800

    Add basic arithmetic functions

---
+++ scripts.js
@@ -0,0 +1,50 @@
+// --- ADDITION ---
+
+function add(x,y) {
+	let sum = x + y;
+	console.log(sum);
+}
+// returns undefined
+
+// add(3,4);
+// 7
+// returns undefined
+
+
+// --- SUBTRACTION ---
+
+function subtract(x,y) {
+	let difference = x - y;
+	console.log(difference);
+}
+// returns undefined
+
+// subtract(10, 6);
+// 4
+// returns undefined
+
+
+// --- MULTIPLICATION ---
+
+function multiply(x,y) {
+	let product = x * y;
+	console.log(product);
+}
+// returns undefined
+
+// multiply(3, 2);
+// 6
+// returns undefined
+
+
+// --- DIVISION ---
+
+function divide(x, y) {
+	let quotient = x / y;
+	console.log(quotient);
+}
+// returns undefined
+
+// divide(10, 5);
+// 2
+// returns undefined
