cfa5236dec226a38c668b104ed8d754764f6e671
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2025-02-01 17:19:53 -0800

    Drafting display update functionality

--- scripts.js
+++ scripts.js
@@ -49,12 +49,21 @@ function divide(x, y) {
 // 2
 // returns undefined
 
+
+// --- GLOBAL VARIABLES ---
+
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
+
+display.innerText = 0;
+let operand1 = null;
+let operand2 = null;
+let operator1 = null;
+let operator2 = null;
+let result = null;
+
 // --- OPERATE ---
 // 1/27/2025: This section in progress. 
-
-let operand1;
-let operand2;
-let operator;
 
 function operate(operation, num1, num2) {
     if (operation == '*') {
@@ -69,3 +78,39 @@ function operate(operation, num1, num2) {
 }
 
 operate(operator, operand1, operand2);
+
+
+// --- DISPLAY FUNCTIONS ---
+// 2/1/2025: In progress
+
+function buttonClick() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", updateDisplay);
+    }
+
+}
+
+buttonClick();
+ 
+function updateDisplayDraft1() {
+    const display = document.getElementById('display');
+    display.innerText = displayValue;
+    if(displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
+
+function updateDisplayDraft2(button) {  
+    let button = buttons[i];
+    // when button is clicked display its content
+    display.innerText = button.innerText;
+}
+
+function clearDisplay() {
+    display.innerText = "0";
+    operand1 = null;
+    operand2 = null;
+    operator1 = null;
+    operator2 = null;
+    result = null;
+}
