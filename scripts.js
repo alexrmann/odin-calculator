576d80511a5c7f1a058e30f126b5e1d04d7724d2
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2025-02-02 16:32:30 -0800

    Revise button classes and ids; update css; add button value attributes; build operand, click, and update display; add operator click error message

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@
 function add(x,y) {
 	let sum = x + y;
 	console.log(sum);
+    return sum;
 }
 // returns undefined
 
@@ -16,6 +17,7 @@ function add(x,y) {
 function subtract(x,y) {
 	let difference = x - y;
 	console.log(difference);
+    return difference;
 }
 // returns undefined
 
@@ -29,6 +31,7 @@ function subtract(x,y) {
 function multiply(x,y) {
 	let product = x * y;
 	console.log(product);
+    return product;
 }
 // returns undefined
 
@@ -42,6 +45,7 @@ function multiply(x,y) {
 function divide(x, y) {
 	let quotient = x / y;
 	console.log(quotient);
+    return quotient;
 }
 // returns undefined
 
@@ -55,62 +59,123 @@ function divide(x, y) {
 const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 
-display.innerText = 0;
-let operand1 = null;
-let operand2 = null;
-let operator1 = null;
-let operator2 = null;
+let displayValue = '0';
+let currentOperand = null;
+let previousOperand = null;
+let currentOperator = null;
+let previousOperator = null;
+let operation = null;
 let result = null;
 
-// --- OPERATE ---
-// 1/27/2025: This section in progress. 
+// --- INITIALIZE CALCULATOR ---
 
-function operate(operation, num1, num2) {
-    if (operation == '*') {
-        multiply(num1, num2);
-    } else if (operation == '/') {
-        divide(num1, num2);
-    } else if (operation == '+') {
-        add(num1, num2);
-    } else {
-        subtract(num1, num2);
-    }
-}
-
-
+updateDisplay();
 
 // --- DISPLAY FUNCTIONS ---
 // 2/1/2025: In progress
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", updateDisplay);
+function updateDisplay() {
+    // Update the display
+    display.innerText = displayValue;
+    
+    // Prevent display text overflow
+    if (displayValue.length > 12) {
+        display.innerText = displayValue.substring(0, 12);
+    }
 }
 
-function updateDisplay() {
-    // Assign the clicked button text to variable
-    let displayValue = this.innerText;
-    console.log(displayValue);
+function clearDisplay() {
+    displayValue = "0";
+    currentOperand = null;
+    previousOperand = null;
+    currentOperator = null;
+    previousOperator = null;
+    result = null;
+}
 
-    // Prevent display text overflow
-    if (displayValue.length > 9) {
-        display.innerText = displayValue.substring(0, 9);
+// --- INTERACTIVITY ---
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+        if (buttons[i].classList.contains('digit')) {
+            inputOperand(buttons[i].value);
+            updateDisplay();
+        } else if (buttons[i].classList.contains('operator')) {
+            inputOperator(buttons[i].value);
+        } else if (buttons[i].id == 'equals') {
+            inputEquals();
+            updateDisplay();
+        } else if (buttons[i].id == 'decimal') {
+            inputDecimal(buttons[i].value);
+            updateDisplay();
+        } else if (buttons[i].id == 'sign') {
+            inputSign(displayValue);
+            updateDisplay();
+        } else if (buttons[i].id == 'clear')
+            clearDisplay();
+            updateDisplay();
+    });
+}
+
+function inputOperand(value) {
+
+    // 2/2/2025: Need to remove bug where pressing 'Zero' button results in a leading '0' on the display.
+    
+    if (currentOperand == null) {
+        currentOperand = value;
+    } else {
+        currentOperand = currentOperand.concat(value);
+    }
+    
+    displayValue = currentOperand;
+    console.log(`Input: ${value}\nDisplay: ${displayValue}`);
+}
+
+function inputOperator(value) {
+    
+    if (currentOperand == null) {
+        displayValue = 'ERROR';
+    }   else if (currentOperator == null) {
+        currentOperator = value;
+    }
+        else {
+        previousOperator = currentOperator;
+        currentOperator = value;
+    }
+        
+    console.log(`Input: ${value}\nDisplay: ${displayValue}`);
+}
+
+
+function inputEquals() {
+    operate(currentOperator, firstOperand, lastOperand);
+    displayValue = result;
+    console.log(`Display: ${displayValue}`);
+}
+
+// IN PROGRESS
+function inputSign(value) {
+    console.log(`Input: ${value}\nDisplay: ${displayValue}`);
+}
+function inputDecimal(value) {
+    console.log(`Input: ${value}\nDisplay: ${displayValue}`);
+}
+
+// --- OPERATE ---
+// 1/27/2025: This section in progress. 
+
+function operate(operation, operand1, operand2) {
+
+    if (operation == 'plus') {
+        result = multiply(operand1, operand2);
+    } else if (operation == 'divide') {
+        result = divide(operand1, operand2);
+    } else if (operation == 'add') {
+        result = add(operand1, operand2);
+    } else {
+        result = subtract(operand1, operand2);
     }
 
-    display.innerText = displayValue;
-}
-
-/*
-function updateDisplayDraft2(button) {  
-    let button = buttons[i];
-    // when button is clicked display its content
-    display.innerText = button.innerText;
-}
-*/
-function clearDisplay() {
-    display.innerText = "0";
-    operand1 = null;
-    operand2 = null;
-    operator1 = null;
-    operator2 = null;
-    result = null;
+    console.log(result);
+    return result;
 }
